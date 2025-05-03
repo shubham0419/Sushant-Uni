a63f444b0bbe05c5e0017d36438aad6bdce70e43
shubham0419 <shubham.e21@nsut.ac.in>
2025-05-03 20:29:50 +0530

    done

--- mainApp/backend/controller/user.controller.js
+++ mainApp/backend/controller/user.controller.js
@@ -21,4 +21,58 @@ const updateUser = async (req,res)=>{
   }
 }
 
-module.exports = {getUser,updateUser}
+const toggleWishlist = async (req,res)=>{
+  try {
+    const {productId} = req.params;
+
+    const user = await User.findById(req.user._id);
+
+    const isProduct = user.wishList.find(prod=>{
+      return prod==productId;
+    })
+
+    const userid = req.user._id;
+    let updatedProduct;
+
+    if(isProduct){
+      updatedProduct = await User.updateOne({_id:userid},{$pull:{wishList:productId}})
+    }else{
+      const newWishList = user.wishList;
+      newWishList.push(productId);
+      updatedProduct = await User.updateOne({_id:userid},{wishList:newWishList})
+    }
+
+    res.status(200).json({message:"product added to whislist",updatedProduct});
+  } catch (error) {
+    res.status(403).json({message:error.message})
+  }
+}
+
+const toggleCart = async (req,res)=>{
+  try {
+    const {productId} = req.params;
+
+    const user = await User.findById(req.user._id);
+
+    const isProduct = user.cart.find(prod=>{
+      return prod==productId;
+    })
+
+    const userid = req.user._id;
+    let updatedProduct;
+
+    if(isProduct){
+      updatedProduct = await User.updateOne({_id:userid},{$pull:{cart:productId}})
+    }else{
+      const newCart = user.cart;
+      newCart.push(productId);
+      updatedProduct = await User.updateOne({_id:userid},{cart:newCart})
+    }
+
+    res.status(200).json({message:"product added to Cart",updatedProduct});
+  } catch (error) {
+    res.status(403).json({message:error.message})
+  }
+}
+
+module.exports = {getUser,updateUser,toggleWishlist,toggleCart}
--- mainApp/backend/routes/user.routes.js
+++ mainApp/backend/routes/user.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const User = require("../model/user.model");
-const { getUser, updateUser } = require("../controller/user.controller");
+const { getUser, updateUser, toggleWishlist, toggleCart } = require("../controller/user.controller");
 const router = express.Router();
 
 router.get("/:id",getUser);
 router.put("/:id",updateUser);
+router.get("/wishlist/:productId",toggleWishlist)
+router.get("/cart/:productId",toggleCart)
 
 module.exports = router;
